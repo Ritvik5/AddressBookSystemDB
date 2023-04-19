be99065efad30c76c4d597bf92c60a398905db23
Ritvik5 <sharma.ritvik56@gmail.com>
2023-04-20 01:12:34 +0530

    Deleting contacts using Person's Name

--- AddressBookSystemDB/AddressBookSystemDataBase.sql
+++ AddressBookSystemDB/AddressBookSystemDataBase.sql
@@ -16,6 +16,8 @@ PhoneNumber BigInt,
 Email Varchar(100)
 );
 
+select * from AddressBook_System;
+
 insert into AddressBook_System values
 ('Ritvik','Sharma','Street No. 64','Bangalore','Karnataka',560056,8888800000,'Abcd@gmail.com'),
 ('Babu','Bhaiya','Street No. 65','Bangalore','Karnataka',560056,8888877777,'Babu@gmail.com'),
@@ -23,3 +25,9 @@ insert into AddressBook_System values
 ('Shyam','Shetty','Street No. 69','Bangalore','Karnataka',560056,8888822222,'Raju@gmail.com')
 
 update AddressBook_System set PhoneNumber = 8888875757 where FirstName = 'Babu';
+update AddressBook_System set PhoneNumber = 9999999999 where FirstName = 'Shyam';
+update AddressBook_System set PhoneNumber = 9898989898 where FirstName = 'Ram';
+
+
+delete from AddressBook_System where FirstName = 'Ram';
+delete from AddressBook_System where FirstName = 'Shyam';
