edaf03bcb72a29ddaf6d674fa06a3286ddb67e33
Ritvik5 <sharma.ritvik56@gmail.com>
2023-04-20 00:48:19 +0530

    Creating Table inside AddressBook DB

--- AddressBookSystemDB/AddressBookSystemDataBase.sql
+++ AddressBookSystemDB/AddressBookSystemDataBase.sql
@@ -4,5 +4,15 @@ select Name from sys.databases;
 
 use AddressBook;
 
-
+create table AddressBook_System
+(
+FirstName Varchar(100) not null,
+LastName Varchar(100) not null,
+Address Varchar(100),
+City Varchar(50),
+State Varchar(50),
+Zip int,
+PhoneNumber BigInt,
+Email Varchar(100)
+);
 
