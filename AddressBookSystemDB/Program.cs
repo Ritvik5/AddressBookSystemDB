efedb23f7b55867338ec9eb33e6ffefff6b019e9
Ritvik5 <sharma.ritvik56@gmail.com>
2023-04-19 23:59:02 +0530

    Welcome to AddressBook System DataBase

---
+++ AddressBookSystemDB/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBookSystemDB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook System DataBase");
+        }
+    }
+}
