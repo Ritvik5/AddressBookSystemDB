9e74f0e2a15b1f37d8c8081df3122b821494485d
Ritvik5 <sharma.ritvik56@gmail.com>
2023-04-20 01:00:04 +0530

    Inserting New Contacts in AddressBook DB

--- AddressBookSystemDB/AddressBookSystemDataBase.sql
+++ AddressBookSystemDB/AddressBookSystemDataBase.sql
@@ -16,3 +16,9 @@ PhoneNumber BigInt,
 Email Varchar(100)
 );
 
+insert into AddressBook_System values
+('Ritvik','Sharma','Street No. 64','Bangalore','Karnataka',560056,8888800000,'Abcd@gmail.com'),
+('Babu','Bhaiya','Street No. 65','Bangalore','Karnataka',560056,8888877777,'Babu@gmail.com'),
+('Ram','Kumar','Street No. 67','Bangalore','Karnataka',560056,8888811111,'Sham@gmail.com'),
+('Shyam','Shetty','Street No. 69','Bangalore','Karnataka',560056,8888822222,'Raju@gmail.com')
+
